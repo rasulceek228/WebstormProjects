780e918922a93c2b7275b94271fe0c230cfc4832
rasulceek0228 <A.Mukhutdinov@ventionteams.com>
2025-05-10 16:34:36 +0500

    fix: updated version of what I have done previous time

---
+++ 03-piiza-menu/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Fuck with your soul like Ether!!!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
